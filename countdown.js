c13dd9160aed963b1a8f9997eb50767097c9c016
evie.szoda <evie.szoda@gmail.com>
2024-10-22 11:15:53 +0100

    Create a reusable function for updating elements updateDaysUntil() and refactor f1() f2() to use it, update names for f1()f2()

--- countdown.js
+++ countdown.js
@@ -5,19 +5,19 @@ function calculateDaysUntil(targetDate) {
     return Math.round((target - today) / (1000 * 60 * 60 * 24));
   }  
 
-function f1() {
-    var daysUntilChristmas = calculateDaysUntil("2024-12-25");
-  
-    var christmasElement = document.getElementById("my-element");
-    christmasElement.innerText = daysUntilChristmas;
+function updateDaysUntil(elementId, targetDate) {
+  var daysUntil = calculateDaysUntil(targetDate);
+  var element = document.getElementById(elementId);
+  element.innerText = daysUntil;
+}
+
+function updateDaysUntilChristmas() {
+    updateDaysUntil("my-element", "2024-12-25");
   }
   
-  function f2() {
-    var daysUntilNewYear = calculateDaysUntil("2025-01-01");
-  
-    var newYearElement = document.getElementById("my-element2");
-    newYearElement.innerText = daysUntilNewYear;
+function updateDaysUntilNewYear() {
+    updateDaysUntil("my-element2", "2025-01-01");
   }
   
-  f1();
-  f2();
+  updateDaysUntilChristmas();
+  updateDaysUntilNewYear();
