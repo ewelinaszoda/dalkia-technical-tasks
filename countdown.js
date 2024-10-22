e0605a9236374ec88fddb86c1d8da9fc1035ba19
evie.szoda <evie.szoda@gmail.com>
2024-10-22 11:11:28 +0100

    Refactor f1() and f2() to use new function calculateDaysUntil

--- countdown.js
+++ countdown.js
@@ -6,23 +6,17 @@ function calculateDaysUntil(targetDate) {
   }  
 
 function f1() {
-    var today = new Date();
-    var target = new Date("2024-12-25");
-  
-    var variable = Math.round((target - today) / (1000 * 60 * 60 * 24));
+    var daysUntilChristmas = calculateDaysUntil("2024-12-25");
   
     var christmasElement = document.getElementById("my-element");
-    christmasElement.innerText = variable;
+    christmasElement.innerText = daysUntilChristmas;
   }
   
   function f2() {
-    var today = new Date();
-    var target = new Date("2025-01-01");
-  
-    var variable = Math.round((target - today) / (1000 * 60 * 60 * 24));
+    var daysUntilNewYear = calculateDaysUntil("2025-01-01");
   
     var newYearElement = document.getElementById("my-element2");
-    newYearElement.innerText = variable;
+    newYearElement.innerText = daysUntilNewYear;
   }
   
   f1();
