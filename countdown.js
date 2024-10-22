9364b47ee36187372bfd4ffaee684f52bf28720b
evie.szoda <evie.szoda@gmail.com>
2024-10-22 21:21:32 +0100

    Store all countdown information in arrayof object adding Valentine's day data, simplify update countdowns to one function

--- countdown.js
+++ countdown.js
@@ -1,8 +1,10 @@
 const MILLISECONDS_PER_DAY = 1000 * 60 * 60 * 24;
-const CHRISTMAS_DATE = "2024-12-25";
-const NEW_YEAR_DATE = "2025-01-01";
-const CHRISTMAS_ELEMENT_ID = "my-element";
-const NEW_YEAR_ELEMENT_ID = "my-element2";
+
+const COUNTDOWNS = [
+    { name: "Christmas", date: "2024-12-25", elementId: "my-element" },
+    { name: "New Year", date: "2025-01-01", elementId: "my-element2" },
+    { name: "Valentine's Day", date: "2025-02-14", elementId: "my-element3" }
+];
 
 const calculateDaysUntil = (targetDate) => {
     const today = new Date();
@@ -10,19 +12,12 @@ const calculateDaysUntil = (targetDate) => {
     return Math.round((target - today) / MILLISECONDS_PER_DAY);
   };
   
-  const updateDaysUntil = (elementId, targetDate) => {
-    const daysUntil = calculateDaysUntil(targetDate);
-    const element = document.getElementById(elementId);
-    element.innerText = daysUntil;
-  };
+  const updateCountdowns = () => {
+    COUNTDOWNS.forEach(countdown => {
+        const daysUntil = calculateDaysUntil(countdown.date);
+        const element = document.getElementById(countdown.elementId);
+        element.textContent = daysUntil;
+    });
+};
   
-  const updateDaysUntilChristmas = () => {
-    updateDaysUntil(CHRISTMAS_ELEMENT_ID, CHRISTMAS_DATE);
-  };
-  
-  const updateDaysUntilNewYear = () => {
-    updateDaysUntil(NEW_YEAR_ELEMENT_ID, NEW_YEAR_DATE);
-  };
-  
-  updateDaysUntilChristmas();
-  updateDaysUntilNewYear();
+updateCountdowns();
