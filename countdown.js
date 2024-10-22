6682869574aaf6be127084759bbce2e3e9047dfb
evie.szoda <evie.szoda@gmail.com>
2024-10-22 11:07:45 +0100

    Improve variable names and create a reusable function for date calculation

--- countdown.js
+++ countdown.js
@@ -1,21 +1,28 @@
+
+function calculateDaysUntil(targetDate) {
+    var today = new Date();
+    var target = new Date(targetDate);
+    return Math.round((target - today) / (1000 * 60 * 60 * 24));
+  }  
+
 function f1() {
-    var p = new Date();
-    var q = new Date("2024-12-25");
+    var today = new Date();
+    var target = new Date("2024-12-25");
   
-    var variable = Math.round((q - p) / (1000 * 60 * 60 * 24));
+    var variable = Math.round((target - today) / (1000 * 60 * 60 * 24));
   
-    var variable2 = document.getElementById("my-element");
-    variable2.innerText = variable;
+    var christmasElement = document.getElementById("my-element");
+    christmasElement.innerText = variable;
   }
   
   function f2() {
-    var p = new Date();
-    var q = new Date("2025-01-01");
+    var today = new Date();
+    var target = new Date("2025-01-01");
   
-    var variable = Math.round((q - p) / (1000 * 60 * 60 * 24));
+    var variable = Math.round((target - today) / (1000 * 60 * 60 * 24));
   
-    var variable2 = document.getElementById("my-element2");
-    variable2.innerText = variable;
+    var newYearElement = document.getElementById("my-element2");
+    newYearElement.innerText = variable;
   }
   
   f1();
